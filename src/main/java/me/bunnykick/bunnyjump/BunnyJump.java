a291b78f4ff74cd44934abb9e20f31c275fb7b44
Droetsch <bunnykick007@googlemail.com>
2023-06-16 23:12:58 +0200

    Initial commit

---
+++ src/main/java/me/bunnykick/bunnyjump/BunnyJump.java
@@ -0,0 +1,17 @@
+package me.bunnykick.bunnyjump;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BunnyJump extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/bunnykick/bunnyjump/configs/ConfigManager.java
@@ -0,0 +1,4 @@
+package me.bunnykick.bunnyjump.configs;
+
+public class ConfigManager {
+}
